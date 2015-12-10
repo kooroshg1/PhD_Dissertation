34edbbdf49504576c9d7bba4243565d51cda9f28
kooroshg1 <kooroshg1@gmail.com>
2015-12-10 15:39:00 -0500

    Started working on Chapter 3

---
+++ Code/python/Chap2_CSAvsAnalytical.py
@@ -0,0 +1,46 @@
+__author__ = 'koorosh'
+import numpy as np
+import matplotlib.pyplot as plt
+# -----------------------------
+font = {'family' : 'monospace',
+        'weight' : 'normal',
+        'size'   : 52}
+plt.rc('font', **font)
+linewidth = 9.0
+markersize = 20
+# -----------------------------
+n = 8
+x = np.linspace(0, 1, n)
+dx = x[2] - x[1]
+T0 = 0
+Tn = 1
+
+# # 2nd Order Central difference
+# Assembling stiffness matrix
+L = np.zeros([n-2, n-2])
+L[0, 0] = -2
+L[0, 1] = 1
+L[-1, -2] = 1
+L[-1, -1] = -2
+for iRow in range(1, len(L)-1):
+    L[iRow, iRow-1] = 1
+    L[iRow, iRow] = -2
+    L[iRow, iRow+1] = 1
+# Assembling boundary condition
+F = np.zeros([n-2, 1])
+F[0] = T0
+F[-1] = -(Tn - T0) / (x[-1] - x[0])
+
+dTdb = np.linalg.solve(L, -F)
+dTdb = np.append(dTdb, -1)
+dTdb = np.append(0, dTdb)
+
+plt.figure(figsize=(30,15))
+plt.plot(x, dTdb, 'k',
+         x, -x, 'wo',
+         ms=20, mew=7, mec='r', lw=linewidth)
+plt.legend(['CSA', 'Analytical'], loc='best')
+plt.xlabel('X')
+plt.ylabel('dT/db')
+plt.savefig('CSA_vs_analytical.eps', format='eps', dpi=1000, bbox_inches='tight')
+plt.show()
