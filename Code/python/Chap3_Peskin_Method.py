c01ef1bc802f4778f237097126790aea6ac6f89b
kooroshg1 <kooroshg1@gmail.com>
2015-12-16 20:02:23 -0500

    Chapter 3

---
+++ Code/python/Chap3_Peskin_Method.py
@@ -0,0 +1,186 @@
+__author__ = 'koorosh'
+import numpy as np
+import matplotlib.pyplot as plt
+# -----------------------------
+font = {'family' : 'monospace',
+        'weight' : 'normal',
+        'size'   : 52}
+plt.rc('font', **font)
+linewidth = 5.0
+markersize = 15
+# -----------------------------
+def phi2(r):
+    y = np.zeros(len(r))
+    for ir in range(0, len(r)):
+        if np.abs(r[ir]) <= 1:
+            y[ir] = 1 - np.abs(r[ir])
+    return y
+def phi3(r):
+    y = np.zeros(len(r))
+    for ir in range(0, len(r)):
+        if np.abs(r[ir]) <= 0.5:
+            y[ir] = (1 + np.sqrt(-3 * r[ir]**2 + 1)) / 3
+        elif np.abs(r[ir]) <= 1.5:
+            y[ir] = (5 - 3 * np.abs(r[ir]) - np.sqrt(-3 * (1 - np.abs(r[ir]))**2 + 1)) / 6
+    return y
+def phi4(r):
+    y = np.zeros(len(r))
+    for ir in range(0, len(r)):
+        if np.abs(r[ir]) <= 1.0:
+            y[ir] = (3 - 2 * np.abs(r[ir]) + np.sqrt(1 + 4 * np.abs(r[ir]) - 4 * r[ir]**2)) / 8
+        elif np.abs(r[ir]) <= 1.5:
+            y[ir] = (5 - 2 * np.abs(r[ir]) - np.sqrt(-7 + 12 * np.abs(r[ir]) - 4 * r[ir]**2)) / 8
+    return y
+def phi6(r):
+    if type(r) is np.float64:
+        r = np.array([r])
+    y = np.zeros(len(r))
+    for ir in range(0, len(r)):
+        if np.abs(r[ir]) <= 1.0:
+            y[ir] = 61.0 / 112.0 \
+                    - 11/42 * np.abs(r[ir]) \
+                    - 11/56 * np.abs(r[ir])**2 \
+                    + 1/12 * np.abs(r[ir])**3 \
+                    + np.sqrt(3)/336 * np.sqrt(243 + 1584 * np.abs(r[ir]) - 784 * np.abs(r[ir])**2
+                                               - 1560 * np.abs(r[ir])**3 + 500 * np.abs(r[ir])**4
+                                               + 336 * np.abs(r[ir])**5 - 112 * np.abs(r[ir])**6)
+        elif np.abs(r[ir]) <= 2.0:
+            y[ir] = 21/16 + 7/12 * np.abs(r[ir]) - 7/8 * np.abs(r[ir])**2 + 1/6 * np.abs(r[ir])**3 - \
+                    3/2 * phi6(np.abs(r[ir]) - 1)
+        elif np.abs(r[ir]) <= 3:
+            y[ir] = 9/8 - 23/12 * np.abs(r[ir]) + 3/4 * np.abs(r[ir])**2 - 1/12 * np.abs(r[ir])**3 + \
+                    1/2 * phi6(np.abs(r[ir]) - 2)
+    return y
+def genDelta(x, x0, type='2point'):
+    dx = x[2] - x[1]
+    r = (x - x0) / dx
+    if type == '2point':
+        out = phi2(r) / dx
+    elif type == '3point':
+        out = phi3(r) / dx
+    elif type == '4point':
+        out = phi4(r) / dx
+    elif type == '6point':
+        out = phi6(r) / dx
+    else:
+        print('Not available')
+        out = 0
+    return out
+def genL(nx):
+    # # 2nd Order Central difference
+    # Assembling stiffness matrix
+    L = np.zeros([nx, nx])
+    L[0, 0] = -2
+    L[0, 1] = 1
+    L[-1, -2] = 1
+    L[-1, -1] = -2
+    for iRow in range(1, len(L)-1):
+        L[iRow, iRow-1] = 1
+        L[iRow, iRow] = -2
+        L[iRow, iRow+1] = 1
+    return L
+# -----------------------------
+nx = 100
+x = np.linspace(0, 1, nx+1).reshape(-1, 1)
+x = x[:-1]
+dx = x[2] - x[1]
+dt = 0.5
+
+U0 = 1
+Un = 0
+u = np.zeros([nx, 1])
+u[0] = U0
+u[-1] = Un
+
+L = genL(nx)
+L = L[1:-1, :]
+un = u
+unp1 = np.zeros([nx, 1])
+unp1[0] = U0
+unp1[-1] = Un
+err = 1.0
+
+f = 0
+K = 0.000
+X0 = 0.817
+Xn = X0
+delta = genDelta(x[1:-1], Xn, type='2point').reshape(-1, 1)
+
+counter = 0
+while err > 1e-5:
+    counter += 1
+    unp1[1:-1] = un[1:-1] + dt * (L.dot(un) - f)
+    U = np.trapz(delta * un[1:-1], x[1:-1], axis=0)
+    Xnp1 = Xn + dt * U
+    F = K * (Xnp1 - X0)
+    f = F * delta
+    Xn = Xnp1
+    if counter > 5000:
+        break
+    err = np.max(np.abs(unp1[1:-1] - un[1:-1]))
+    un[1:-1] = unp1[1:-1]
+
+skip = 3
+plt.figure(figsize=(30, 15))
+plt.plot(x, unp1, 'k',
+         x[::skip], -x[::skip]/X0+1, 'wo',
+         lw=linewidth, mew=linewidth, ms=markersize)
+plt.xlim([0, X0])
+plt.ylim([0, 1])
+plt.xlabel('X')
+plt.ylabel('Response (u)')
+plt.legend(['IB', 'Analytical'])
+plt.savefig('peskin_method_0817.eps', format='eps', dpi=1000, bbox_inches='tight')
+# figsize=(30, 15)
+plt.figure(figsize=(30, 15))
+plt.plot(x, np.abs(np.divide(unp1+x/X0-1, -x/X0+1)), 'k',
+         lw=linewidth, mew=linewidth, ms=markersize)
+plt.xlim([0, X0])
+plt.xlabel('X')
+plt.ylabel('Percentage of Error')
+plt.savefig('err_peskin_method_0817.eps', format='eps', dpi=1000, bbox_inches='tight')
+plt.show()
+
+# # ----------------------------
+# # Solver
+# nx = 8
+# x = np.linspace(0, 1, nx)
+# dx = x[2] - x[1]
+# nt = 10
+# dt = 0.5
+#
+# U0 = 1
+# Un = 0
+# u = np.zeros([nx, 1])
+# u[0] = U0
+# u[-1] = Un
+#
+# # # 2nd Order Central difference
+# # Assembling stiffness matrix
+# def genL(nx):
+#     L = np.zeros([nx, nx])
+#     L[0, 0] = -2
+#     L[0, 1] = 1
+#     L[-1, -2] = 1
+#     L[-1, -1] = -2
+#     for iRow in range(1, len(L)-1):
+#         L[iRow, iRow-1] = 1
+#         L[iRow, iRow] = -2
+#         L[iRow, iRow+1] = 1
+#     return L
+#
+# L = genL(nx)
+# L = L[1:-1, :]
+# un = u
+# unp1 = np.zeros([nx, 1])
+# unp1[0] = U0
+# unp1[-1] = Un
+# err = 1.0
+# while err > 1e-5:
+#     unp1[1:-1] = un[1:-1] + dt * L.dot(un)
+#     err = np.max(np.abs(unp1[1:-1] - un[1:-1]))
+#     un[1:-1] = unp1[1:-1]
+#
+# plt.figure()
+# plt.plot(x, unp1)
+# plt.show()
--- Code/python/Chap3_regularizedDeltaFunctions.py
+++ Code/python/Chap3_regularizedDeltaFunctions.py
@@ -1,15 +1,23 @@
 __author__ = 'koorosh'
 import numpy as np
 import matplotlib.pyplot as plt
-
-def phi1(r):
+# -----------------------------
+font = {'family' : 'monospace',
+        'weight' : 'normal',
+        'size'   : 52}
+plt.rc('font', **font)
+linewidth = 9.0
+markersize = 20
+# -----------------------------
+def phi2(r):
     y = np.zeros(len(r))
     for ir in range(0, len(r)):
         if np.abs(r[ir]) <= 1:
             y[ir] = 1 - np.abs(r[ir])
     return y
 
-def phi2(r):
+
+def phi3(r):
     y = np.zeros(len(r))
     for ir in range(0, len(r)):
         if np.abs(r[ir]) <= 0.5:
@@ -18,7 +26,8 @@ def phi2(r):
             y[ir] = (5 - 3 * np.abs(r[ir]) - np.sqrt(-3 * (1 - np.abs(r[ir]))**2 + 1)) / 6
     return y
 
-def phi3(r):
+
+def phi4(r):
     y = np.zeros(len(r))
     for ir in range(0, len(r)):
         if np.abs(r[ir]) <= 1.0:
@@ -27,7 +36,10 @@ def phi3(r):
             y[ir] = (5 - 2 * np.abs(r[ir]) - np.sqrt(-7 + 12 * np.abs(r[ir]) - 4 * r[ir]**2)) / 8
     return y
 
-def phi4(r):
+
+def phi6(r):
+    if type(r) is np.float64:
+        r = np.array([r])
     y = np.zeros(len(r))
     for ir in range(0, len(r)):
         if np.abs(r[ir]) <= 1.0:
@@ -40,21 +52,46 @@ def phi4(r):
                                                + 336 * np.abs(r[ir])**5 - 112 * np.abs(r[ir])**6)
         elif np.abs(r[ir]) <= 2.0:
             y[ir] = 21/16 + 7/12 * np.abs(r[ir]) - 7/8 * np.abs(r[ir])**2 + 1/6 * np.abs(r[ir])**3 - \
-                    3/2 * phi4(np.abs(r[ir]) - 1)
+                    3/2 * phi6(np.abs(r[ir]) - 1)
         elif np.abs(r[ir]) <= 3:
             y[ir] = 9/8 - 23/12 * np.abs(r[ir]) + 3/4 * np.abs(r[ir])**2 - 1/12 * np.abs(r[ir])**3 + \
-                    1/2 * phi4(np.abs(r[ir]) - 2)
+                    1/2 * phi6(np.abs(r[ir]) - 2)
     return y
 
 
-def delta(x, x0, type):
+def delta(x, x0, type='2point'):
     dx = x[2] - x[1]
     r = (x - x0) / dx
-    out = phi4(r) / dx
+    if type == '2point':
+        out = phi2(r) / dx
+    elif type == '3point':
+        out = phi3(r) / dx
+    elif type == '4point':
+        out = phi4(r) / dx
+    elif type == '6point':
+        out = phi6(r) / dx
+    else:
+        print('Not available')
+        out = 0
     return out
 
-x = np.linspace(-3, 3, 100)
-# print(np.trapz(delta(x, 1, 2), x))
-plt.figure()
-plt.plot(x, delta(x, 1.3541, 2))
+x = np.linspace(-3, 3, 101)
+dx = x[2] - x[1]
+r = (x - 0) / dx
+d2 = delta(x, 0, '2point') / np.max(delta(x, 0, '2point'))
+d3 = delta(x, 0, '3point') / np.max(delta(x, 0, '2point'))
+d4 = delta(x, 0, '4point') / np.max(delta(x, 0, '2point'))
+d6 = delta(x, 0, '6point') / np.max(delta(x, 0, '2point'))
+plt.figure(figsize=(30, 15))
+plt.plot(r, d2,
+         r, d3,
+         r, d4,
+         r, d6,
+         lw=linewidth)
+plt.legend(['2Point', '3Point', '4Point', '6Point'])
+plt.xlim([-5, 5])
+plt.xlabel('r')
+plt.ylabel(r'$\phi(r)$')
+plt.grid('on')
+plt.savefig('phi_function.eps', format='eps', dpi=1000, bbox_inches='tight')
 plt.show()
