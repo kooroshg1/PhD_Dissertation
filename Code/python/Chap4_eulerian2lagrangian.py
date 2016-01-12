10aee06df470ef307777a2ba843cfb1d2a184b58
kooroshg1 <kooroshg1@gmail.com>
2016-01-11 19:37:45 -0500

    Working on chapter 4

--- Code/python/Chap4_eulerian2lagrangian.py
+++ Code/python/Chap4_eulerian2lagrangian.py
@@ -9,7 +9,7 @@ font = {'family' : 'monospace',
         'size'   : 52}
 plt.rc('font', **font)
 linewidth = 5.0
-markersize = 15
+markersize = 25
 # -----------------------------
 x, t = sym.symbols('x t', real=True)
 
@@ -29,12 +29,13 @@ D3 = sym.lambdify([x, t], D3, "numpy")
 D4 = sym.lambdify([x, t], D4, "numpy")
 
 x = np.linspace(0, 1, 9)
+print(x)
 y = lambda x: x**2
 # X = 0.3321
 # t = 0.1
 
 X = 0.5813
-t = 0.12
+t = 0.1
 
 xt = x - X
 Dbar1 = D1(x - X, t) / np.max(np.abs(D1(x - X, t)))
@@ -43,19 +44,24 @@ print(np.trapz(D4(x - X, t), x))
 print(np.trapz(np.multiply(y(x), D4(x - X, t)), x))
 print(y(X))
 
-# np.savetxt('Lagrange.txt', Dbar1 * np.trapz(np.multiply(y(x), D1(x - X, t)), x))
+fileName = 'euler2lagrange_example_03321.eps'
 dx = x[2] - x[1]
 # plt.figure()
 # plt.plot(x, D4(xt, t))
 # plt.figure()
 # plt.plot(x, Dbar4)
-plt.figure()
-plt.plot(x, Dbar4 * np.trapz(np.multiply(y(x), D4(x - X, t)), x), 'k--o',
+plt.figure(figsize=(30, 15))
+plt.plot(x, Dbar4 * np.trapz(np.multiply(y(x), D4(x - X, t)), x), 'g--s',
          x, y(x), 'k-o',
          X, np.trapz(np.multiply(y(x), D4(x - X, t)), x), 'ro',
          ms=markersize, lw=linewidth)
 plt.hold('on')
 plt.plot(X, y(X), 'b+',
          mew=linewidth, ms=markersize)
-# plt.xlim([X - 3*dx, X + 3*dx])
+plt.xlabel(r'$x$')
+plt.ylabel(r'$y(x)$')
+plt.grid('on')
+plt.minorticks_on()
+plt.grid(which='minor')
+# plt.savefig(fileName, format='eps', dpi=1000, bbox_inches='tight')
 plt.show()
