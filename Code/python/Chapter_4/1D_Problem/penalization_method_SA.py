e2458d9b260a134a757d025bfef04067314b4f97
kooroshg1 <kooroshg1@gmail.com>
2016-01-19 20:46:16 -0500

    Sensitivity analysis for 1D problem

--- Code/python/Chapter_4/1D_Problem/penalization_method_SA.py
+++ Code/python/Chapter_4/1D_Problem/penalization_method_SA.py
@@ -1,33 +1,34 @@
 __author__ = 'koorosh'
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy as sym
 # -----------------------------
-# font = {'family' : 'monospace',
-#         'weight' : 'normal',
-#         'size'   : 52}
-# plt.rc('font', **font)
-# linewidth = 5.0
-# markersize = 15
 font = {'family' : 'monospace',
         'weight' : 'normal',
-        'size'   : 22}
+        'size'   : 52}
 plt.rc('font', **font)
-linewidth = 2.0
+linewidth = 5.0
 markersize = 15
+# font = {'family' : 'monospace',
+#         'weight' : 'normal',
+#         'size'   : 22}
+# plt.rc('font', **font)
+# linewidth = 2.0
+# markersize = 15
 # -----------------------------
-def H(x, eta=1, x0=0):
+def H(x, eta=1.0, x0=0):
     y = np.zeros([len(x), 1])
+    x = x - x0
     for ix in range(0, len(x)):
-        if x[ix] - x0 >= 0:
-            y[ix] = 1
+        y[ix] = (1 + np.tanh(x[ix] / eta)) / 2.0
     return y
 
-# def H(x, eta=1.0, x0=0):
-#     y = np.zeros([len(x), 1])
-#     x = x - x0
-#     for ix in range(0, len(x)):
-#         y[ix] = (1 + np.tanh(x[ix] / eta)) / 2.0
-#     return y
+def D(x, eta=1.0, x0=0):
+    y = np.zeros([len(x), 1])
+    x = x - x0
+    for ix in range(0, len(x)):
+        y[ix] = (1 - (np.tanh(x[ix] / eta))**2.0) / (2.0 * eta)
+    return y
 
 def genL(nx):
     # # 2nd Order Central difference
@@ -43,26 +44,48 @@ def genL(nx):
         L[iRow, iRow+1] = 1
     return L
 
-nx = 81
+# x = np.linspace(-3, 3, 1000)
+# dx = x[2] - x[1]
+# p = 0.99
+# R = 5*dx
+# eta0 = R / (np.arctanh(np.sqrt(p)))
+#
+# print(np.trapz(D(x, eta=eta0, x0=0), x, axis=0))
+
+
+nx = 120
 x = np.linspace(0, 1, nx).reshape(-1, 1)
 dx = x[2] - x[1]
 dt = 1e-4
 
 L = genL(nx) / dx**2
 
-U = 1000
+U = 1
 BC = np.zeros([nx - 2, 1])
 BC[0] = U
 
 un = np.zeros([nx, 1])
 un[0] = U
 kappa = 2/dt
-Xn = 0.4327
+Xn = 0.7583
 eta0 = dx / (2 * np.arctanh(0.99))
 
 xInd = np.argmax(x > Xn) - 1
 uAnal = -BC[0] * x / Xn + BC[0]
+suAnal = U * x / Xn**2
 
+sU = 0
+sBC = np.zeros([nx - 2, 1])
+sBC[0] = sU
+
+sun = np.zeros([nx, 1])
+sun[0] = sU
+
+p = 0.99
+eta0 = 4 * dx / (2 * np.arctanh(p))
+print(np.trapz(D(x[1:-1], eta=eta0, x0=Xn), x[1:-1], axis=0))
+
+# # Solving governing equations
 # plt.figure()
 for it in range(1, 40000):
     RHS1 = BC * dt / (2 * dx**2)
@@ -78,6 +101,23 @@ for it in range(1, 40000):
         print(it)
         break
     un = np.append(np.append(BC[0], unp1), 0).reshape(-1, 1)
+
+    sRHS1 = sBC * dt / (2 * dx**2)
+    sRHS2 = np.eye(nx - 2, nx - 2).dot(sun[1:-1])
+    sRHS3 = L[1:-1, :].dot(sun) * dt / 2
+    sf1 = -kappa * np.multiply(sun[1:-1], H(x[1:-1], eta=eta0, x0=Xn)) * dt
+    sf2 = -kappa * np.multiply(un[1:-1], D(x[1:-1], eta=eta0, x0=Xn)) * -1 * dt
+    sf = sf1 + sf2
+    # print(u)
+    sRHS = sRHS1 + sRHS2 + sRHS3 + sf
+    A = np.eye(nx - 2, nx - 2) - L[1:-1, 1:-1] * dt / 2
+    sunp1 = np.linalg.solve(A, sRHS)
+    serr = np.max(np.abs(sunp1 - sun[1:-1]))
+    # if serr < 1e-10:
+    #     print(it)
+    #     break
+    sun = np.append(np.append(sBC[0], sunp1), 0).reshape(-1, 1)
+
     # plt.plot(x, un,
     #          x, -BC[0] * x / Xn + BC[0], 'r')
     # plt.title(str(it))
@@ -92,20 +132,15 @@ for it in range(1, 40000):
 
 rmsd = np.linalg.norm(un[:xInd] - uAnal[:xInd]) / (np.sqrt(len(un[:xInd])) * (np.max(np.abs(uAnal)) -
                                                                               np.min(np.abs(uAnal))))
-# np.savetxt('uIB_Hf_n161.txt', un)
-# np.savetxt('uAnal.txt', uAnal)
 print(rmsd)
-# skip = 1
-# # plt.figure()
-# plt.figure(figsize=(30, 15))
-# plt.plot(x, un, 'k',
-#          x[::skip], -BC[0] * x[::skip] / Xn + BC[0], 'wo',
-#          lw=linewidth, mew=linewidth, ms=markersize)
-# plt.xlabel('X')
-# plt.ylabel('Response (u)')
-# plt.legend(['IB', 'Analytical'])
-# plt.xlim([0, Xn])
-# plt.ylim([0, U])
-# plt.grid('on')
-# plt.show()
-# #
+
+rmsd = np.linalg.norm(sun[:xInd] - suAnal[:xInd]) / (np.sqrt(len(sun[:xInd])) * (np.max(np.abs(suAnal)) -
+                                                                                 np.min(np.abs(suAnal))))
+print(rmsd)
+
+plt.figure()
+plt.plot(x, sun, 'k',
+         x, U*x/Xn**2, 'wo',
+         ms=markersize, lw=linewidth, mew=linewidth)
+plt.xlim([Xn-0.1, Xn])
+plt.show()
--- Code/python/Chapter_4/1D_Problem/penalization_method_postprocess.py
+++ Code/python/Chapter_4/1D_Problem/penalization_method_postprocess.py
@@ -1,6 +1,7 @@
 __author__ = 'koorosh'
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize as sop
 
 # -----------------------------
 font = {'family' : 'monospace',
@@ -37,3 +38,48 @@ markersize = 15
 # plt.savefig(fileName, format='eps', dpi=1000, bbox_inches='tight')
 # plt.show()
 # -----------------------------
+
+# -----------------------------
+# RSME for RH vs. number of nodes
+n = np.array([20, 40, 80, 120, 160, 180, 320])
+
+# # x_wall = 0.4321, U = 1
+# RSME_GE = np.array([0.1071337784, 0.0330065688, 0.0060771245, 0.004709103, 0.0030466798, 0.0038251288, 0.0046022806])
+# RSME_SA = np.array([0.1371056254, 0.0356610516, 0.0183384031, 0.0063932112, 0.0046022806, 0.0057421065, 0.0046022806])
+
+# # x_wall = 0.7583, U = 1
+RSME_GE = np.array([0.0847649007, 0.0256031798, 0.0046463731, 0.003475902, 0.0023105673, 0.0028755434, 0.0044625426])
+RSME_SA = np.array([0.1713840201, 0.0539913891, 0.0102838446, 0.0107751846, 0.0053758169, 0.0054250107, 0.0070518279])
+
+
+
+def func(x, a, b):
+        return a * x**b
+popt, pcov = sop.curve_fit(f=func, xdata=n, ydata=RSME_GE)
+aGE = popt[0]
+bGE = popt[1]
+print(bGE)
+popt, pcov = sop.curve_fit(f=func, xdata=n, ydata=RSME_SA)
+aSA = popt[0]
+bSA = popt[1]
+print(bSA)
+
+fileName = 'penalizationMethod_SA_1D_problem_xw07583.eps'
+plt.figure(figsize=(30, 15))
+plt.loglog(n, RSME_GE, 'k',
+           n, RSME_SA, 'r--',
+           lw=linewidth)
+plt.legend(['GE', 'SA'])
+plt.xlabel('Number of nodes')
+plt.ylabel('RSME')
+plt.grid('on')
+plt.minorticks_on()
+plt.grid(which='minor')
+plt.hold('on')
+plt.loglog(n, aGE*n**bGE, 'k',
+           n, aSA*n**bSA, 'r--', lw=1)
+plt.savefig(fileName, format='eps', dpi=1000, bbox_inches='tight')
+plt.show()
+
+# -----------------------------
+
