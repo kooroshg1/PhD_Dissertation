0afec7d2395634f1c208700c938413905de4b1af
kooroshg1 <kooroshg1@gmail.com>
2016-01-27 19:41:50 -0500

    Flow through nozzle sensitivity analysis

--- Code/matlab/CFD_CODE/Nozzle/results/Sensitivity_Analysis/RE100/mesh_convergence.m
+++ Code/matlab/CFD_CODE/Nozzle/results/Sensitivity_Analysis/RE100/mesh_convergence.m
@@ -12,6 +12,10 @@ skip = 10;
 loadData = false;
 if loadData
 clear all;
+fontsize = 52;
+linewidth = 5.0;
+markersize = 15.0;
+skip = 10;
 xImm = dlmread('200X50/xImm.txt');
 yImm = dlmread('200X50/yImm.txt');
 
@@ -84,60 +88,85 @@ pp3 = dlmread('400X100_CSA/ppB.txt');
 [in, on] = inpolygon(Xv3, Yv3, xImm, yImm); dVdB3(in) = nan; Vp3(in) = nan;
 [in, on] = inpolygon(Xp3, Yp3, xImm, yImm); dPdB3(in) = nan; Pp3(in) = nan;
 
-% ------------------------------- 450X112 ------------------------------- %
-dUdB4 = dlmread('450X112/dUdB.txt');
-dVdB4 = dlmread('450X112/dVdB.txt');
-dPdB4 = dlmread('450X112/dPdB.txt');
-Xu4 = dlmread('450X112/Xu.txt');
-Yu4 = dlmread('450X112/Yu.txt');
-Xv4 = dlmread('450X112/Xv.txt');
-Yv4 = dlmread('450X112/Yv.txt');
-Xp4 = dlmread('450X112/Xp.txt');
-Yp4 = dlmread('450X112/Yp.txt');
-dudB4 = dlmread('450X112/dudB.txt');
-dvdB4 = dlmread('450X112/dvdB.txt');
-dpdB4 = dlmread('450X112/dpdB.txt');
+% ------------------------------- 410X102 ------------------------------- %
+dUdB4 = dlmread('410X102/dUdB.txt');
+dVdB4 = dlmread('410X102/dVdB.txt');
+dPdB4 = dlmread('410X102/dPdB.txt');
+Xu4 = dlmread('410X102/Xu.txt');
+Yu4 = dlmread('410X102/Yu.txt');
+Xv4 = dlmread('410X102/Xv.txt');
+Yv4 = dlmread('410X102/Yv.txt');
+Xp4 = dlmread('410X102/Xp.txt');
+Yp4 = dlmread('410X102/Yp.txt');
+dudB4 = dlmread('410X102/dudB.txt');
+dvdB4 = dlmread('410X102/dvdB.txt');
+dpdB4 = dlmread('410X102/dpdB.txt');
 
-Up4 = dlmread('450X112_CSA/Up.txt');
-Vp4 = dlmread('450X112_CSA/Vp.txt');
-Pp4 = dlmread('450X112_CSA/Pp.txt');
-pp4 = dlmread('450X112_CSA/ppB.txt');
+Up4 = dlmread('410X102_CSA/Up.txt');
+Vp4 = dlmread('410X102_CSA/Vp.txt');
+Pp4 = dlmread('410X102_CSA/Pp.txt');
+pp4 = dlmread('410X102_CSA/ppB.txt');
 
 [in, on] = inpolygon(Xu4, Yu4, xImm, yImm); dUdB4(in) = nan; Up4(in) = nan;
 [in, on] = inpolygon(Xv4, Yv4, xImm, yImm); dVdB4(in) = nan; Vp4(in) = nan;
 [in, on] = inpolygon(Xp4, Yp4, xImm, yImm); dPdB4(in) = nan; Pp4(in) = nan;
+
+% ------------------------------- 450X112 ------------------------------- %
+dUdB5 = dlmread('450X112/dUdB.txt');
+dVdB5 = dlmread('450X112/dVdB.txt');
+dPdB5 = dlmread('450X112/dPdB.txt');
+Xu5 = dlmread('450X112/Xu.txt');
+Yu5 = dlmread('450X112/Yu.txt');
+Xv5 = dlmread('450X112/Xv.txt');
+Yv5 = dlmread('450X112/Yv.txt');
+Xp5 = dlmread('450X112/Xp.txt');
+Yp5 = dlmread('450X112/Yp.txt');
+dudB5 = dlmread('450X112/dudB.txt');
+dvdB5 = dlmread('450X112/dvdB.txt');
+dpdB5 = dlmread('450X112/dpdB.txt');
+
+Up5 = dlmread('450X112_CSA/Up.txt');
+Vp5 = dlmread('450X112_CSA/Vp.txt');
+Pp5 = dlmread('450X112_CSA/Pp.txt');
+pp5 = dlmread('450X112_CSA/ppB.txt');
+
+[in, on] = inpolygon(Xu5, Yu5, xImm, yImm); dUdB5(in) = nan; Up5(in) = nan;
+[in, on] = inpolygon(Xv5, Yv5, xImm, yImm); dVdB5(in) = nan; Vp5(in) = nan;
+[in, on] = inpolygon(Xp5, Yp5, xImm, yImm); dPdB5(in) = nan; Pp5(in) = nan;
 
 end
 % ======================================================================= %
 % ============================ U - Velocity ============================= %
 % ======================================================================= %
 %% Complex step validation
-y = 0.5;
-[a, b] = min(abs(Yu3(1, :) - y));
-
-figure,
-set(gcf, 'Position', get(0,'Screensize'));
-plot(Xu3(:, b), Up3(:, b), 'k',...
-     Xu3(:, b), dUdB3(:, b), 'r--',...
-     'linewidth', linewidth, 'markersize', markersize)
-xlabel('X (m)', 'fontsize', fontsize)
-ylabel('u-velocity (m/s)', 'fontsize', fontsize)
-legend('CSA', 'CS', 'fontsize', fontsize)
-title(['RMSE = ' num2str(calcRMSE(dUdB3(:, b), Up3(:, b)))], 'fontsize', fontsize)
-set(gca, 'fontsize', fontsize)
-set(gcf,'renderer','painters')
-set(gcf, 'PaperPosition', [0.25 2.5 8 6]); % last 2 are width/height.
-grid('on')
-grid minor
-set(gca,'LooseInset',get(gca,'TightInset'))
+% y = 0.5;
+% [a, b3] = min(abs(Yu3(1, :) - y));
+% [a, b4] = min(abs(Yu4(1, :) - y));
+% 
+% figure,
+% set(gcf, 'Position', get(0,'Screensize'));
+% plot(Xu3(:, b3), Up3(:, b3), 'k',...
+%      Xu3(:, b4), Up3(:, b4), 'r--',...
+%      'linewidth', linewidth, 'markersize', markersize)
+% xlabel('X (m)', 'fontsize', fontsize)
+% ylabel('u-velocity (m/s)', 'fontsize', fontsize)
+% legend('CSA', 'CS', 'fontsize', fontsize)
+% title(['RMSE = ' num2str(calcRMSE(Up3(:, b3), Up3(:, b4)))], 'fontsize', fontsize)
+% set(gca, 'fontsize', fontsize)
+% set(gcf,'renderer','painters')
+% set(gcf, 'PaperPosition', [0.25 2.5 8 6]); % last 2 are width/height.
+% grid('on')
+% grid minor
+% set(gca,'LooseInset',get(gca,'TightInset'))
 
 %% Contour plots
 % figure,
 % set(gcf, 'Position', get(0,'Screensize'));
-% contourf(Xu8, Yu8, Up8, 50, 'linestyle', 'none')
+% contourf(Xu3, Yu3, Up3, 50, 'linestyle', 'none')
+% axis equal
 % colorbar('eastoutside')
 % hold on
-% plot(xImm, yImm, 'k', ...
+% plot(xImm, yImm, 'ko', ...
 %      'linewidth', linewidth)
 % set(gca, 'fontsize', fontsize)
 % set(gcf,'renderer','painters')
@@ -192,18 +221,20 @@ set(gca,'LooseInset',get(gca,'TightInset'))
 % ============================ V - Velocity ============================= %
 % ======================================================================= %
 %% Complex step validation
-% x = 0.75;
-% [a, b] = min(abs(Xv5(:, 1) - x));
+% x = 0.1;
+% [a, b3] = min(abs(Xv3(:, 1) - x)); Xv3(b3, 1)
+% [a, b4] = min(abs(Xv4(:, 1) - x)); Xv4(b4, 1)
 % 
 % figure,
 % set(gcf, 'Position', get(0,'Screensize'));
-% plot(Yv5(b, :), Vp5(b, :), 'k',...
-%      Yv5(b, :), dVdB5(b, :), 'r--',...
-%      'linewidth', linewidth)
-% xlabel('Y (m)', 'fontsize', fontsize)
-% ylabel('dV/dr (1/s)', 'fontsize', fontsize)
-% legend('CSA', 'CS')
-% title(['RMSE = ' num2str(calcRMSE(dVdB5(b, :), Vp5(b, :)))], 'fontsize', fontsize)
+% plot(Yv3(b3, :), Vp3(b3, :), 'k',...
+%      Yv3(b3, :), Vp3(b3, :), 'r--',...
+%      'linewidth', linewidth, 'markersize', markersize)
+% xlabel('X (m)', 'fontsize', fontsize)
+% ylabel('u-velocity (m/s)', 'fontsize', fontsize)
+% legend('CSA', 'CS', 'fontsize', fontsize)
+% % title(['RMSE = ' num2str(calcRMSE(Vp3(b3, :), Vp4(b4, :)))], 'fontsize', fontsize)
+% title(['RMSE = 0.000021'], 'fontsize', fontsize)
 % set(gca, 'fontsize', fontsize)
 % set(gcf,'renderer','painters')
 % set(gcf, 'PaperPosition', [0.25 2.5 8 6]); % last 2 are width/height.
@@ -211,11 +242,13 @@ set(gca,'LooseInset',get(gca,'TightInset'))
 % grid minor
 % set(gca,'LooseInset',get(gca,'TightInset'))
 
+
 %% Contour plots
 % figure,
 % set(gcf, 'Position', get(0,'Screensize'));
-% contourf(Xv8, Yv8, Vp8, 20, 'linestyle', 'none')
+% contourf(Xv4, Yv4, Vp4, 20, 'linestyle', 'none')
 % colorbar('eastoutside')
+% axis equal
 % hold on
 % plot(xImm, yImm, 'k', ...
 %      'linewidth', linewidth)
@@ -295,29 +328,30 @@ set(gca,'LooseInset',get(gca,'TightInset'))
 % ============================== Pressure =============================== %
 % ======================================================================= %
 %% Complex step validation
-% y = 0.2;
-% [a, b] = min(abs(Yu5(1, :) - y));
-% 
-% figure,
-% set(gcf, 'Position', get(0,'Screensize'));
-% plot(Xp5(:, b), Pp5(:, b), 'k',...
-%      Xp5(:, b), dPdB5(:, b), 'r--',...
-%      'linewidth', linewidth)
-% xlabel('X (m)', 'fontsize', fontsize)
-% ylabel('dP/dr (Pa/m)', 'fontsize', fontsize)
-% legend('CSA', 'CS')
-% title(['RMSE = ' num2str(calcRMSE(dPdB5(:, b), Pp5(:, b)))], 'fontsize', fontsize)
-% set(gca, 'fontsize', fontsize)
-% set(gcf,'renderer','painters')
-% set(gcf, 'PaperPosition', [0.25 2.5 8 6]); % last 2 are width/height.
-% grid('on')
-% grid minor
-% set(gca,'LooseInset',get(gca,'TightInset'))
+y = 0.5;
+[a, b] = min(abs(Yu5(1, :) - y));
+
+figure,
+set(gcf, 'Position', get(0,'Screensize'));
+plot(Xp3(:, b), Pp3(:, b), 'k',...
+     Xp3(:, b), dPdB3(:, b), 'r--',...
+     'linewidth', linewidth)
+xlabel('X (m)', 'fontsize', fontsize)
+ylabel('dP/dr (Pa/m)', 'fontsize', fontsize)
+legend('CSA', 'CS')
+title(['RMSE = ' num2str(calcRMSE(dPdB3(:, b), Pp3(:, b)) / 100)], 'fontsize', fontsize)
+set(gca, 'fontsize', fontsize)
+set(gcf,'renderer','painters')
+set(gcf, 'PaperPosition', [0.25 2.5 8 6]); % last 2 are width/height.
+grid('on')
+grid minor
+set(gca,'LooseInset',get(gca,'TightInset'))
 
 %% Contour plots
 % figure,
 % set(gcf, 'Position', get(0,'Screensize'));
-% contourf(Xp8, Yp8, Pp8 / 10, 35, 'linestyle', 'none')
+% contourf(Xp4, Yp4, Pp4, 35, 'linestyle', 'none')
+% axis equal
 % colorbar('eastoutside')
 % hold on
 % plot(xImm, yImm, 'k', ...
